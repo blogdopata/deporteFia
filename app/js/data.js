ae7534051cacffdd97011f7a898905932d755448
blogdopata <vcaballero_c@hotmail.com>
2017-10-13 22:09:08 -0500

    Portada done

--- app/js/data.js
+++ app/js/data.js
@@ -1,8 +1,8 @@
 var codropsEvents = {
 	//'12-25-YYYY' : '<span><h3>¡Feliz NavidadxD!</h3></span>',
-	'10-13-YYYY' : '<span>Inauguración Interfacultades </span>',
+	//'10-13-YYYY' : '<span>Inauguración Interfacultades </span>',
 	//'01-01-YYYY' : '<span><h3>¡Feliz Año Nuevo!</h3></span>',
-	'10-17-2017' : '<span>Inicio Futbol</span>',
+	'10-14-2017' : '<span>Inauguración Interfacultades</span>',
 	//'10-14-2017' : '<span><h3>Inicio de Torneo</h3>  </span>',
 //	'11-DD-2017' : {content : '<span><h3>Empezaron los torneos</h3></span>', startDate : 1, endDate : 10},
 };
